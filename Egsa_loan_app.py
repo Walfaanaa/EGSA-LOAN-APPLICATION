76dbbc02eb6a2b651b4fbfe0fee7dc4e5d7efe94
Walfaanaa Magarsaa Ejeta <119677853+Walfaanaa@users.noreply.github.com>
2026-07-21 10:53:27 +0300

    Update Egsa_loan_app.py

--- Egsa_loan_app.py
+++ Egsa_loan_app.py
@@ -292,110 +292,85 @@ if page=="Apply for Loan":
 
     st.write("Complete all information below.")
 
-    # -----------------------------
-    # Personal Information
-    # -----------------------------
-
-    st.subheader("Personal Information")
-
-    full_name = st.text_input("Full Name")
-
-    national_id = st.text_input("National ID")
-
-    phone = st.text_input("Phone Number")
-
-    staff_status = st.selectbox(
-        "Staff Status",
-        [
-            "Permanent",
-            "Contract",
-            "Temporary",
-            "Other"
-        ]
-    )
-
-    monthly_salary = st.number_input(
-        "Monthly Salary",
-        min_value=0.0,
-        step=100.0
-    )
-
-
-    st.divider()
+       st.divider()
 
 
     # -----------------------------
-# Loan Information
-# -----------------------------
+    # Loan Information
+    # -----------------------------
 
-st.subheader("Loan Information")
-
-
-loan_amount = st.number_input(
-    "Loan Amount",
-    min_value=0.0,
-    value=0.0,
-    step=100.0,
-    key="loan_amount_input"
-)
+    st.subheader("Loan Information")
 
 
-duration = st.number_input(
-    "Loan Duration (Months)",
-    min_value=1,
-    max_value=60,
-    value=12,
-    step=1,
-    key="loan_duration_input"
-)
-
-
-# Convert values
-loan_amount = float(loan_amount)
-duration = int(duration)
-
-
-# Calculate Loan
-interest_rate, interest_amount, monthly_payment, total_payment = calculate_loan(
-    loan_amount,
-    duration
-)
-
-
-st.success(
-    f"Interest Rate : {interest_rate}%"
-)
-
-
-col1, col2 = st.columns(2)
-
-
-with col1:
-
-    st.metric(
-        "Interest Amount",
-        f"{interest_amount:,.2f} ETB"
-    )
-
-
-    st.metric(
-        "Monthly Payment",
-        f"{monthly_payment:,.2f} ETB"
-    )
-
-
-with col2:
-
-    st.metric(
+    loan_amount = st.number_input(
         "Loan Amount",
-        f"{loan_amount:,.2f} ETB"
+        min_value=0.0,
+        value=0.0,
+        step=100.0,
+        key="loan_amount_input"
     )
 
 
-    st.metric(
-        "Total Repayment",
-        f"{total_payment:,.2f} ETB"
+    duration = st.number_input(
+        "Loan Duration (Months)",
+        min_value=1,
+        max_value=60,
+        value=12,
+        step=1,
+        key="loan_duration_input"
     )
+
+
+    # Convert values
+
+    loan_amount = float(loan_amount)
+
+    duration = int(duration)
+
+
+
+    # Calculate Loan
+
+    interest_rate, interest_amount, monthly_payment, total_payment = calculate_loan(
+        loan_amount,
+        duration
+    )
+
+
+    st.success(
+        f"Interest Rate : {interest_rate}%"
+    )
+
+
+    col1, col2 = st.columns(2)
+
+
+    with col1:
+
+        st.metric(
+            "Interest Amount",
+            f"{interest_amount:,.2f} ETB"
+        )
+
+
+        st.metric(
+            "Monthly Payment",
+            f"{monthly_payment:,.2f} ETB"
+        )
+
+
+    with col2:
+
+        st.metric(
+            "Loan Amount",
+            f"{loan_amount:,.2f} ETB"
+        )
+
+
+        st.metric(
+            "Total Repayment",
+            f"{total_payment:,.2f} ETB"
+        )
 
 
 # -----------------------------
